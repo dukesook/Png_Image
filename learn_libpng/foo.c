82d2b232c3795a9d2697046a85978d24bab8f846
dev sook <devonsookhoo14@gmail.com>
2022-03-06 18:23:56 -0700

    Simplifying main() A basic version of the code is working, but everything is in the main function. I am working to move most of the logic to "png_to_hex.c". I will also add commandLineHandler.c/.h & getopt.c/.h

---
+++ learn_libpng/foo.c
@@ -0,0 +1,7 @@
+
+#include "foo.h"
+#include <stdio.h>
+
+void foo1() {
+	printf("foo1\n");
+}
---
+++ learn_libpng/main.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <png.h>
+
+#include "foo.h"
+
+int main() {
+	printf("testing\n");
+
+	foo1();
+
+	return 0;
+}
